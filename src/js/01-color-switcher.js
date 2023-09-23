8debfe1afd3b267a59ab08faa4d1246e47fe70b1
Pantherstich <pantherstich@gmail.com>
2023-09-23 22:40:58 +0300

    forst blood

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const backgtound = document.querySelector('body');
 
+startButton.addEventListener('click', clickStart);
+stopButton.addEventListener('click', clickStop);
+
+let Change = null;
+stopButton.disabled = true;
+
+function clickStart() {
+stopButton.disabled = false;
+startButton.disabled = true;
+Change = setInterval(bgColorChange, 1000)
+}
+function clickStop() {
+stopButton.disabled = true;
+startButton.disabled = false;
+    clearInterval(Change);
+}
+function bgColorChange() {
+    backgtound.style.backgroundColor = getRandomHexColor()
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}

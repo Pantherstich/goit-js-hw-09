f6953ac33739cd82d11e202bf6e14b369c19c51a
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-10-01 04:08:22 +0000

    Deploying to gh-pages from @ Pantherstich/goit-js-hw-09@85f2b402e845e8dec64f1d11defa155a031240fc 🚀

--- 01-color-switcher.d03dc679.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,Change=setInterval(d,1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(Change)}));function d(){a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.disabled=!0;
-//# sourceMappingURL=01-color-switcher.d03dc679.js.map
